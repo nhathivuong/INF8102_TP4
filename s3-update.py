39bf74fa330bded59a5a48dfc8150b7998c09153
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2025-11-26 19:39:44 -0500

    adding the update to the config file of the s3 bucket (and the script to push the update)

---
+++ s3-update.py
@@ -0,0 +1,10 @@
+import boto3
+
+client = boto3.client('cloudformation')
+
+response = client.update_stack(
+    StackName='s3-stack-2',
+    TemplateURL='https://polystudents3-460783431840.s3.ca-central-1.amazonaws.com/s3-2.json',
+    Parameters=[
+    ],
+)
